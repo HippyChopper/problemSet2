cd23042fa7de7d4404b9b180b05d1de864a92ab5
t00202274 <patrick.riordan@students.ittralee.ie>
2018-10-09 15:31:59 +0100

    update

--- src/bikeTest.java
+++ src/bikeTest.java
@@ -5,17 +5,29 @@ public class bikeTest {
 
     public static void main(String[] args) {
         JTextArea textArea = new JTextArea("text area \n");
-        Font font = new Font("monospaced",Font.PLAIN,10);
+        Font font = new Font("monospaced", Font.PLAIN, 10);
         textArea.setFont(font);
 
         Bicycle bike1 = new Bicycle();
 
-       bike1.setName(bike1.getName());
+        bike1.setName(bike1.getName());
 
     }
 
-    public static String getName(){
+    public static String getName() {
         String name = JOptionPane.showInputDialog("Please enter a name");
         return name;
+    }
+
+    public static String getMake() {
+        String make = JOptionPane.showInputDialog("Please enter a make");
+        return make;
+
+    }
+
+    public static int getvalue() {
+        int value = Integer.parseInt(JOptionPane.showInputDialog("Please enter a make"));
+        return value;
+
     }
 }
